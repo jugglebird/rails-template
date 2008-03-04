4d40f97246f21e61b7dc34aaf4f729370a4d1841
Michael Larocque <mlarocque@prolumina.com>
2008-03-04 16:34:15 -0700

    first commit

---
+++ web/app/controllers/main_controller.rb
@@ -0,0 +1,8 @@
+class MainController < ApplicationController
+
+  def index
+  end
+
+  def about
+  end
+end
---
+++ web/spec/controllers/main_controller_spec.rb
@@ -0,0 +1,24 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe MainController do
+
+  #Delete these examples and add some real ones
+  it "should use MainController" do
+    controller.should be_an_instance_of(MainController)
+  end
+
+
+  describe "GET 'index'" do
+    it "should be successful" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+end
